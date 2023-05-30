b389e2f16f8a982026ec37ed0f007d68888008f3
devmoaid1 <moaidmohamed1312@gmail.com>
2023-05-30 20:53:36 +0300

    added app assets

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,17 @@
+abstract class AppAssets {
+  static const homeIcon = "assets/icons/home.svg";
+  static const searchIcon = "assets/icons/search.svg";
+  static const sofaIcon = "assets/icons/sofa.svg";
+  static const bedIcon = "assets/icons/bed.svg";
+  static const deleteIcon = "assets/icons/delete.svg";
+  static const emailIcon = "assets/icons/email.svg";
+  static const loginIcon = "assets/icons/login.svg";
+  static const logoutIcon = "assets/icons/logout.svg";
+  static const passwordIcon = "assets/icons/password.svg";
+  static const settingsIcon = "assets/icons/settings.svg";
+  static const tableIcon = "assets/icons/table.svg";
+  static const userIcon = "assets/icons/user.svg";
+  static const noSearch = "assets/icons/no_search.svg";
+  static const featureGraphics = "assets/icons/password.svg";
+  static const gamingChair = "assets/icons/gaming_chair.svg";
+}
